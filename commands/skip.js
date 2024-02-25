f822fe1b636f5a2b3e9b360f8395884ec4652a4b
r0nii <100228504+r0nii@users.noreply.github.com>
2024-02-25 01:43:39 +0100

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,18 +1,3 @@
-/*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-*/
 const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
 const db = require("../mongoDB");
 module.exports = {
@@ -31,27 +16,27 @@ module.exports = {
     try {
 
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: 'No music', ephemeral: true }).catch(e => { })
 
       let number = interaction.options.getNumber('number');
       if (number) {
-        if (!queue.songs.length > number) return interaction.reply({ content: '⚠️ Exceeded current no of songs', ephemeral: true }).catch(e => { })
-        if (isNaN(number)) return interaction.reply({ content: '⚠️ Invalid Number', ephemeral: true }).catch(e => { })
-        if (1 > number) return interaction.reply({ content: '⚠️ Invalid Number', ephemeral: true }).catch(e => { })
+        if (!queue.songs.length > number) return interaction.reply({ content: 'Exceeded current no of songs', ephemeral: true }).catch(e => { })
+        if (isNaN(number)) return interaction.reply({ content: 'Invalid Number', ephemeral: true }).catch(e => { })
+        if (1 > number) return interaction.reply({ content: 'Invalid Number', ephemeral: true }).catch(e => { })
 
         try {
         let old = queue.songs[0];
         await client.player.jump(interaction, number).then(song => {
-          return interaction.reply({ content: `⏯️ Skipped : **${old.name}**` }).catch(e => { })
+          return interaction.reply({ content: `Skipped : **${old.name}**` }).catch(e => { })
         })
       } catch(e){
-        return interaction.reply({ content: '❌ Queue is empty!!', ephemeral: true }).catch(e => { })
+        return interaction.reply({ content: 'Empty queue', ephemeral: true }).catch(e => { })
       }
       } else {
 try {
   const queue = client.player.getQueue(interaction.guild.id);
   if (!queue || !queue.playing) {
-    return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+    return interaction.reply({ content: 'No music', ephemeral: true });
   }
 
   let old = queue.songs[0];
@@ -60,16 +45,14 @@ try {
   const embed = new EmbedBuilder()
     .setColor('#3498db')
     .setAuthor({
-      name: 'Song Skipped',
-      iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157269773118357604/giphy.gif?ex=6517fef6&is=6516ad76&hm=f106480f7d017a07f75d543cf545bbea01e9cf53ebd42020bd3b90a14004398e&',
-      url: 'https://discord.gg/FUEHs7RCqz'
+      name: 'Skipped',
     })
-    .setDescription(success ? ` **SKIPPED** : **${old.name}**` : '❌ Queue is empty!')
+    .setDescription(success ? ` **SKIPPED** : **${old.name}**` : 'Empty queue')
     .setTimestamp();
 
   return interaction.reply({ embeds: [embed] });
 }catch (e) {
-          return interaction.reply({ content: '❌ Queue is empty!!', ephemeral: true }).catch(e => { })
+          return interaction.reply({ content: 'Empty queue', ephemeral: true }).catch(e => { })
         }
       }
 
@@ -78,18 +61,3 @@ try {
   }
   },
 };
-/*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-*/
