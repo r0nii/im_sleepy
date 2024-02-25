94dc18fbf23ee2df02efa235dc946587e1795ffd
r0nii <100228504+r0nii@users.noreply.github.com>
2024-02-25 06:08:33 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -3,16 +3,23 @@ module.exports = {
   ownerID: ["1004206704994566164", ""],
   botInvite: "",
   supportServer: "",
-  status: 'sleepy',
+  mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
+  status: 'RTXX GG',
   commandsDir: './commands',
   language: "en",
   embedColor: "00fbff",
   errorLog: "",
 
+
+  sponsor: {
+    status: true,
+    url: "https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A",
+  },
+
   voteManager: {
     status: false,
     api_key: "",
-    vote_commands: ["back", "channel", "clear", "dj", "filter", "loop", "nowplaying", "pause", "playnormal", "playlist", "queue", "resume", "save", "play",  "skip", "stop", "time", "volume"],
+    vote_commands: ["back", "channel", "clear", "dj", "filter", "loop", "nowplaying", "pause", "playnormal", "playlist", "queue", "resume", "save", "play", "skip", "stop", "time", "volume"],
     vote_url: "",
   },
 
