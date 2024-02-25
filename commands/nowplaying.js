ad986026bb2c9c8df02661982f5cf44f7e9c6285
r0nii <100228504+r0nii@users.noreply.github.com>
2024-02-25 04:23:15 +0100

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -2,7 +2,7 @@ const { EmbedBuilder } = require('discord.js');
 const db = require("../mongoDB");
 module.exports = {
   name: "nowplaying",
-  description: "get info of current song.",
+  description: "get info of current song",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
