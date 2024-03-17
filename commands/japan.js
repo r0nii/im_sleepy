29a6402a7944ff6e618782b326f2af519ebfa1e3
Marcin0Przedwojewski <49309504+Marcin0Przedwojewski@users.noreply.github.com>
2024-03-18 00:31:42 +0100

    zarys tlumaczenia w komendzie japan.js
    dodano plan jak bedzie wyglodalo tlymaczenie, uzywajac ascii.
    nie testowano, trzeba poprawic skladnie.
    dodano komentarze

--- commands/japan.js
+++ commands/japan.js
@@ -13,16 +13,34 @@ module.exports = {
   voiceChannel: true,
   run: async (client, interaction) => {
     
+    //pobieram podana opcje do zmiennej namet
     let namet=interaction.options.getString('string');
+    //tworze slownik do tlumaczenia  cheat_sheet, gdzie 'a' = cheat_sheet[0], 'b' = cheat_sheet[1], etc...
+    const String cheat_sheet[] = {"ka", "zu", "mi", "te", "ku", "lu", "Ji", "Ri", "Ki", "ZU","me", "Ta", "Rin", "To", "Mo", "NO", "ke", "shi", "Ari", "chi", "do", "ru", "mei", "Na", "fu", "z"}; 
     try {
 		const tlumaczony = new String("");
+		//uposledzone for( int zmienna = 0 ; zmienna< namet.length() ; zmienna ++ ), moze "for_each"
 		let zmien = new int(0);
+		//dla kazdego znaku w tlumaczonym slowie...
 		while( zmien < namet.length() )
 		{
-			tlumaczony = tlumaczony + "." + namet[zmien];
+			char tmp = namet[i];
+			//ustalam ze duze litery tlumacze tak samo jak male
+			if(tmp >= 'A' && tmp <= 'Z')
+			{
+				tmp = (tmp - 'A') + 'a';
+			}
+			//sprawdzam czy znak to litera
+			if(tmp >= 'a' && tmp <= 'z')
+				tlumaczony = tlumaczony + cheat__sheet[tmp-'a'];
+			//jesli nie to nie zmieniam
+			else
+				tlumaczony = tlumaczony +  tmp;
+			//ide do nastepnego znaku
 			zmien++;
 		}
 
+	//jakas odpowiedz 
   const embed = new EmbedBuilder()
     .setColor('#3498db')
     .setAuthor({
