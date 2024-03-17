8b0d232262beda96a1e160c43c41665681c0692d
Marcin0Przedwojewski <49309504+Marcin0Przedwojewski@users.noreply.github.com>
2024-03-17 23:43:39 +0100

    fixing japan.js
    it should swap given string into it with '.' in between each letter for now

--- commands/japan.js
+++ commands/japan.js
@@ -13,50 +13,27 @@ module.exports = {
   voiceChannel: true,
   run: async (client, interaction) => {
     
+    String namet=interaction.options.getString('string');
     try {
-/*
-      const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: 'No music', ephemeral: true }).catch(e => { })
+		String tlumaczony = "";
+		for(int i= 0 ; i < namet.length() ; i++)
+		{
+			tlumaczony = tlumaczony + "." + namet[i];
+		}
 
-      let number = interaction.options.getNumber('number');
-      if (number) {
-        if (!queue.songs.length > number) return interaction.reply({ content: 'Exceeded current no of songs', ephemeral: true }).catch(e => { })
-        if (isNaN(number)) return interaction.reply({ content: 'Invalid Number', ephemeral: true }).catch(e => { })
-        if (1 > number) return interaction.reply({ content: 'Invalid Number', ephemeral: true }).catch(e => { })
-
-        try {
-        let old = queue.songs[0];
-        await client.player.jump(interaction, number).then(song => {
-          return interaction.reply({ content: `Skipped : **${old.name}**` }).catch(e => { })
-        })
-      } catch(e){
-        return interaction.reply({ content: 'Empty queue', ephemeral: true }).catch(e => { })
-      }
-      } else {
-try {
-  const queue = client.player.getQueue(interaction.guild.id);
-  if (!queue || !queue.playing) {
-    return interaction.reply({ content: 'Nothing to skip', ephemeral: true });
-  }
-
-  let old = queue.songs[0];
-  const success = await queue.skip();
-*/
   const embed = new EmbedBuilder()
     .setColor('#3498db')
     .setAuthor({
       name: 'translating',
     })
-    .setDescription(` before translation : **${this.name}**`);
+    .setDescription(` before translation : **${namet}**, after **${tlumaczony}**`);
 
   return interaction.reply({ embeds: [embed] });
-}catch (e) {
-          return interaction.reply({ content: 'Empty queue, skipping aborted', ephemeral: true }).catch(e => { })
-        }
-      }
+
 
     } catch (e) {
     console.error(e); 
   }
   },
 };
+
