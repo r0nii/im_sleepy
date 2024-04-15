20208ad89a2cb57604da85041cfe98b22bfd84dd
r0nii <100228504+r0nii@users.noreply.github.com>
2024-04-15 02:30:06 +0200

    Add files via upload

--- events/player/addList.js
+++ events/player/addList.js
@@ -1,5 +1,6 @@
 const db = require("../../mongoDB");
 module.exports = async (client, queue, playlist) => {
 
-queue?.textChannel?.send({ content: `<@${playlist.user.id}>, \`${playlist.name} (${playlist.songs.length + " " + ''})\` ` }).catch(e => { })
+  
+  
 }
