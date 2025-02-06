8417e27a9639f40f8de1a6ef1060044905bccf91
r0nii <100228504+r0nii@users.noreply.github.com>
2025-02-06 20:01:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -31,3 +31,14 @@ client.login(client.config.app.token).catch(async (e) => {
         console.error('Token ERROR: 404');
     }
 });
+
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  const imagePath = path.join(__dirname, 'index.html');
+  res.sendFile(imagePath);
+});
+app.listen(port, () => {
+  console.log(`Listening to: http://localhost:${port}`);
+});
