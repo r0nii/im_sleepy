49ae1ed7e9d090cd014841f6991d0344cdc3a00e
r0nii <100228504+r0nii@users.noreply.github.com>
2025-02-06 21:13:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     app: {
         token: process.env.DISCORD_TOKEN || 'xxx',
-        playing: 'by the Community ❤️',
+        playing: 'Let me sleep',
         global: true,
         guild: process.env.GUILD_ID || 'xxx',
         extraMessages: false,
